280659ee35e5db5885866812a6209edca1008e8f
Naveen Gopi <naveen8194.g@gmail.com>
2018-02-07 12:55:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def processRequest(req):
         page = manager.request('GET', base_url)
         soup = BeautifulSoup(page.data, 'html.parser')
         print('After Parameter function')
-        for sibling in soup.find(id=introduction).next_siblings:
+        for sibling in soup.find(id='introduction').next_siblings:
             if sibling.name is None:
                 continue
             elif sibling.name == 'p':
