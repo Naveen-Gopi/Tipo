41aabda2e58b00040fb1f42b4f768e25275dac3f
Naveen Gopi <naveen8194.g@gmail.com>
2018-02-21 20:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def makeWebhookParameters(req):
     for (entity_name, entity_value) in parameters.items():
         if len(entity_value) > 0:
             tipo_id = entity_name
-	    print('Inside the funtion makeWebhookParameters',tipo_id)
+            print('Inside the funtion makeWebhookParameters', tipo_id)
     if tipo_id is None:
         return None
 
@@ -128,5 +128,6 @@ if __name__ == '__main__':
     print('Starting app on port %d' % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
+
 
 			
